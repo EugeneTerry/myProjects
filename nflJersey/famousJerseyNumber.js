6a4a9a9a37540e48a7f0f7b7392d8db61e7ca40d
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-28 22:36:18 -0600

    Create famousJerseyNumber.js

---
+++ nflJersey/famousJerseyNumber.js
@@ -0,0 +1,17 @@
+
+var btn = document.getElementById("btn");
+ 
+  var testObj = {
+    12: "Joe Namath",
+    16: "Joe Montana",
+    19: "Johnny Unitas",
+    22: "Derick Henry",
+    27: "Eddie George"
+};
+
+btn.addEventListener("click", (e)=> {
+  e.preventDefault();
+    var par = parseInt(document.getElementById("par").value);
+  var pi = "The Player's name is " ;
+  document.getElementById("myBook").innerHTML = pi + testObj[par];
+})
