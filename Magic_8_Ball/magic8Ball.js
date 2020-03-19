9ad21514014079ae4fdbee5d3497d978165c08d0
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-18 21:43:21 -0500

    Create magic8Ball.js

---
+++ Magic_8_Ball/magic8Ball.js
@@ -0,0 +1,46 @@
+
+let randomPick = (Math.floor(Math.random() * 100));
+console.clear();
+if (randomPick >= 95) {
+    console.log('It is decidedly so');
+  } else if (randomPick >= 90) {
+      console.log('It is certain')
+  } else if (randomPick >= 85) {
+      console.log('Don\'t count on it')
+  } else if (randomPick >= 80) {
+      console.log('Concentrate and ask again')
+  } else if (randomPick >= 75) {
+      console.log('Cannot predict now')
+  } else if (randomPick >= 70) {
+      console.log('Better not tell you now')
+  } else if (randomPick >= 65) {
+      console.log('Ask again later')
+  } else if (randomPick >= 60) {
+      console.log('As I see it, yes')
+  } else if (randomPick >= 55) {
+      console.log('Most likely')
+  } else if (randomPick >= 50) {
+      console.log('My reply is no')
+  } else if (randomPick >= 45) {
+     console.log('My sources say no')
+  } else if (randomPick >= 40) {
+     console.log('Outlook not so good')
+  } else if (randomPick >= 35) {
+     console.log('Outlook good')
+  } else if (randomPick >= 30) {
+      console.log('Reply hazy, try again')
+  } else if (randomPick >= 25) {
+      console.log('Signs point to yes')
+  } else if (randomPick >= 20) {
+      console.log('Very doubtful')
+  } else if (randomPick >= 15) {
+      console.log('Without a doubt.')
+  } else if (randomPick >= 10) {
+      console.log('Yes')
+  } else if (randomPick >= 5) {
+      console.log('Yes – definitely')
+  } else if (randomPick >= 0) {
+      console.log('You may rely on it')  
+  } else {
+      console.log('');
+}
