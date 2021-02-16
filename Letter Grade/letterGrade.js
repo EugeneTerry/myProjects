0f2892268846dde4556d734e8b1eef797e9d9385
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-02-16 14:12:34 -0600

    Create letterGrade.js
    Grading Code

---
+++ Letter Grade/letterGrade.js
@@ -0,0 +1,18 @@
+//this converts a numerical grade to a letter grade
+var grade = prompt("Enter Your Grade");
+function letterGrade(grade) {
+if (grade >= 93) {
+  console.log (`${grade} is an A`);
+}else if (grade >= 86) {
+  console.log (`${grade} is a B`)
+}else if (grade >= 75) {
+  console.log (`${grade} is a C`)
+}else if (grade >= 70) {
+  console.log (`${grade} is a D`)
+}else if (grade >= 0) {
+  console.log (`${grade} is a F`)
+} else {
+  console.log ("Input a real numerical grade")
+  }
+}
+letterGrade(grade);
